00dbd8c8cd2960ff66241738c174c61b2354d937
navono <navono007@gmail.com>
2018-11-26 16:40:26 +0800

    feat: 实现查找，查找结果写入文件

--- cmd/search.go
+++ cmd/search.go
@@ -15,10 +15,22 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"strings"
+	"sync"
 
+	"github.com/360EntSecGroup-Skylar/excelize"
 	"github.com/spf13/cobra"
 )
+
+type searchResult struct {
+	SearchName string `json:"searchName"`
+	Filename   string `json:"filename"`
+	Col        int    `json:"col"`
+	Row        int    `json:"row"`
+}
 
 // searchCmd represents the search command
 var searchCmd = &cobra.Command{
@@ -28,9 +40,18 @@ var searchCmd = &cobra.Command{
 	例如，在同层文件夹中的所有 .xlxs 文件中查找 王五：
 	xxx search -d "." -n "王五"
 	`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("search called")
-	},
+	SilenceUsage: true,
+	// Args: func(cmd *cobra.Command, args []string) error {
+	// 	fmt.Println(args)
+	// 	if len(args) < 1 {
+	// 		return errors.New("至少需要一个参数 -n")
+	// 	}
+	// 	// if myapp.IsValidColor(args[0]) {
+	// 	//   return nil
+	// 	// }
+	// 	return fmt.Errorf("invalid color specified: %s", args[0])
+	// },
+	Run: search,
 }
 
 func init() {
@@ -44,6 +65,88 @@ func init() {
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	searchCmd.Flags().StringP("dir", "d", ".", "xlxs 文件夹")
 	searchCmd.Flags().StringP("name", "n", "", "需要查找的名称")
+	searchCmd.Flags().StringP("dir", "d", ".", "xlxs 文件夹")
+}
+
+func search(cmd *cobra.Command, args []string) {
+	dir, _ := cmd.Flags().GetString("dir")
+	name, _ := cmd.Flags().GetString("name")
+
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	wg := &sync.WaitGroup{}
+
+	result := make(chan interface{})
+
+	for _, file := range files {
+		wg.Add(1)
+		go searchInFile(dir+"/"+file.Name(), name, result, wg)
+	}
+
+	// 监听 WaitGroup
+	go func() {
+		wg.Wait()
+		close(result)
+	}()
+
+	// 合并查询结果到文件中
+	done := make(chan bool, 1)
+	go mergeResultToFile(result, done)
+	<-done
+}
+
+func searchInFile(filename string, name string, resultChan chan<- interface{}, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	groupXlsx, err := excelize.OpenFile(filename)
+	if err != nil {
+		fmt.Printf("读取 %s 错误。%s", filename, err)
+		fmt.Println()
+		return
+	}
+
+	var rows = groupXlsx.GetRows("Sheet1")
+	for rowIdx, row := range rows {
+		for colIdx, colCell := range row {
+			if strings.Compare(name, colCell) != 0 {
+				continue
+			}
+
+			result := &searchResult{
+				SearchName: name,
+				Filename:   filename,
+				Row:        rowIdx + 1,
+				Col:        colIdx + 1,
+			}
+			data, _ := json.MarshalIndent(result, "", "  ")
+			resultChan <- data
+		}
+	}
+}
+
+func mergeResultToFile(result <-chan interface{}, done chan<- bool) {
+	var jsonData []byte
+	for data := range result {
+		bytes, _ := data.([]byte)
+		for idx := range bytes {
+			jsonData = append(jsonData, bytes[idx])
+		}
+
+		fmt.Println(string(jsonData))
+	}
+
+	err := ioutil.WriteFile("result.json", jsonData, 0644)
+	check(err)
+	done <- true
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
